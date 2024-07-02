3a83dbc185808103a15c6d5616a6427d37aba7b8
Sqhh99 <127586242+Sqhh99@users.noreply.github.com>
2024-07-02 14:12:00 +0800

    sql
    Database files

---
+++ book_borrowing_management.sql
@@ -0,0 +1,170 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: book_borrowing_management
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  `author` varchar(50) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `stock` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `chk_stock_nonnegative` CHECK ((`stock` >= 0))
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'算法导论','Fiction','F. Scott Fitzgerald',10.99,100),(2,'计算机组成原理','计算机','阿双',100.00,100),(3,'Qt','Classic','Harper Lee',7.99,2),(4,'C++','Science','Stephen Hawking',15.99,4),(6,'阿凡提','童话','阿双',35.00,999),(8,'大话数据结构','计算机','阿双',67.00,1000),(9,'我的世界','游戏','阿双',98.00,100),(10,'哈哈','搞笑','阿双',20.00,111);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `borrow_records`
+--
+
+DROP TABLE IF EXISTS `borrow_records`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `borrow_records` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `book_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `borrow_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `return_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `book_id` (`book_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `borrow_records_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  CONSTRAINT `borrow_records_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `borrow_records`
+--
+
+LOCK TABLES `borrow_records` WRITE;
+/*!40000 ALTER TABLE `borrow_records` DISABLE KEYS */;
+INSERT INTO `borrow_records` VALUES (10,1,1,'2024-05-14 17:38:54','2024-05-16 16:00:00'),(14,8,1,'2024-05-20 17:23:18','2024-05-22 16:00:00'),(17,1,6,'2024-05-23 16:23:30','2024-05-23 16:00:00'),(18,2,6,'2024-05-23 16:23:34','2024-05-23 16:00:00'),(19,3,6,'2024-05-23 16:23:37','2024-05-23 16:00:00'),(20,4,6,'2024-05-23 16:23:41','2024-05-23 16:00:00'),(21,6,6,'2024-05-23 16:27:06','2024-05-23 16:00:00'),(22,1,6,'2024-05-23 16:38:07','2024-05-29 16:00:00'),(23,1,6,'2024-05-23 16:38:18','2024-05-28 16:00:00'),(24,1,6,'2024-05-23 16:38:27','2024-05-27 16:00:00'),(26,6,6,'2024-05-23 16:42:05','2024-05-23 16:00:00');
+/*!40000 ALTER TABLE `borrow_records` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `trg_after_borrow_insert` AFTER INSERT ON `borrow_records` FOR EACH ROW BEGIN
+    UPDATE books
+    SET stock = stock - 1
+    WHERE id = NEW.book_id;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Temporary view structure for view `borrowuserbook`
+--
+
+DROP TABLE IF EXISTS `borrowuserbook`;
+/*!50001 DROP VIEW IF EXISTS `borrowuserbook`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `borrowuserbook` AS SELECT 
+ 1 AS `id`,
+ 1 AS `username`,
+ 1 AS `name`,
+ 1 AS `borrow_time`,
+ 1 AS `return_time`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password_hash` char(60) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `gender` enum('M','F','O') DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `authority` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Sqhh99','123456','1234567890','M','2024-05-12 11:53:43',1),(2,'李四','1881881818','19928817271','F','2024-05-12 12:43:43',0),(4,'hello','1111','2222','F','2024-05-12 12:49:03',0),(6,'1','1','','M','2024-05-14 11:15:08',1),(14,'张三','123456','1111111111','M','2024-05-24 13:57:01',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Final view structure for view `borrowuserbook`
+--
+
+/*!50001 DROP VIEW IF EXISTS `borrowuserbook`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `borrowuserbook` AS select `borrow_records`.`id` AS `id`,`users`.`username` AS `username`,`books`.`name` AS `name`,`borrow_records`.`borrow_time` AS `borrow_time`,`borrow_records`.`return_time` AS `return_time` from ((`books` join `users`) join `borrow_records`) where ((`borrow_records`.`book_id` = `books`.`id`) and (`borrow_records`.`user_id` = `users`.`id`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-27 17:52:19
